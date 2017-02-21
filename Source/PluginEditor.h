c3c9e53c995db4adb8ece3c3f1fa34a776c65438
Mrober202 <michaelrobertson@Michaels-MacBook-Pro.local>
2017-02-21 20:41:53 +0000

    Setup Editor

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,40 +1,70 @@
 /*
   ==============================================================================
 
-    This file was auto-generated!
+  This is an automatically generated GUI class created by the Projucer!
 
-    It contains the basic framework code for a JUCE plugin editor.
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 4.3.1
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library - "Jules' Utility Class Extensions"
+  Copyright (c) 2015 - ROLI Ltd.
 
   ==============================================================================
 */
 
-#ifndef PLUGINEDITOR_H_INCLUDED
-#define PLUGINEDITOR_H_INCLUDED
+#ifndef __JUCE_HEADER_AF02D40A039D6393__
+#define __JUCE_HEADER_AF02D40A039D6393__
 
+//[Headers]     -- You can add your own extra header files here --
 #include "../JuceLibraryCode/JuceHeader.h"
-#include "PluginProcessor.h"
+//[/Headers]
+
 
 
 //==============================================================================
 /**
+                                                                    //[Comments]
+    An auto-generated component, created by the Projucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
 */
-class AmbiEncoder12AudioProcessorEditor  : public AudioProcessorEditor
+class AmbiEncoder12AudioProcessorEditor  : public AudioProcessorEditor,
+                                           public Timer
 {
 public:
-    AmbiEncoder12AudioProcessorEditor (AmbiEncoder12AudioProcessor&);
+    //==============================================================================
+    AmbiEncoder12AudioProcessorEditor (AmbiEncoder12AudioProcessor& p);
     ~AmbiEncoder12AudioProcessorEditor();
 
     //==============================================================================
-    void paint (Graphics&) override;
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    void timerCallback();
+    //[/UserMethods]
+
+    void paint (Graphics& g) override;
     void resized() override;
 
-private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    AmbiEncoder12AudioProcessor& processor;
 
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    AmbiEncoder12AudioProcessor& processor;
+    //[/UserVariables]
+
+    //==============================================================================
+
+
+    //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AmbiEncoder12AudioProcessorEditor)
 };
 
+//[EndFile] You can add extra defines here...
+//[/EndFile]
 
-#endif  // PLUGINEDITOR_H_INCLUDED
+#endif   // __JUCE_HEADER_AF02D40A039D6393__
